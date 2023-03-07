c2ca14c7a128c01426b36621553d7f70a448433c
Andrii Marchuk <64638375+andriise@users.noreply.github.com>
2023-03-07 13:18:58 +0200

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
+import { PhonebookForm } from './PhonebookForm/PhonebookForm';
+import { PhonebookContacts } from './PhonebookContacts/PhonebookContacts';
+import { Filter } from './Filter/Filter';
+import { Container } from './App.styled';
+export function App() {
+  const [contacts, setContacts] = useState([
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
+  const [filter, setFilter] = useState('');
+
+  const addContact = (name, number) => {
+    const contact = { id: nanoid(), name, number };
+    if (contacts.find(item => item.name.toLowerCase() === name.toLowerCase())) {
+      alert(`${name} is already in contacts`);
+      return;
+    }
+    setContacts([contact, ...contacts]);
+  };
+
+  const deleteContact = id => {
+    setContacts(contacts.filter(item => item.id !== id));
+  };
+
+  const changeFilter = e => {
+    setFilter(e.target.value);
+  };
+
+  const filteredContacts = contacts.filter(item =>
+    item.name.toLowerCase().includes(filter.toLowerCase())
+  );
+  return (
+    <Container>
+      <PhonebookForm onAddContact={addContact} />
+      <Filter value={filter} onChange={changeFilter} />
+      <PhonebookContacts
+        contacts={filteredContacts}
+        onDeleteContact={deleteContact}
+      />
+    </Container>
+  );
+}
